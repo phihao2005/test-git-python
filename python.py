1b97312a4ab4caa1dcfa504a5a0b5531afd3992a
Trinh Nguyen Phi Hao <phihaokc2005@gmail.com>
2026-06-27 22:11:22 +0700

    Update new Python code

--- python.py
+++ python.py
@@ -1,6 +1,7 @@
 print("This is my code")
-a = 50
-b = 100
-c = 2
-print("100/2+50")
-print("Kết quả:",b/c+a)
+
+ten = input("Nhập tên của bạn: ")
+print("Tên là: ",ten)
+
+ngaythangnamsinh = input("Ngày tháng năm sinh: ")
+print("Ngày tháng năm sinh: ",ngaythangnamsinh)
