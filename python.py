58c4bd4bf39236939941a10cafd7e34bf6fd9be8
Trinh Nguyen Phi Hao <phihaokc2005@gmail.com>
2026-06-29 19:28:09 +0700

    update code

--- python.py
+++ python.py
@@ -6,6 +6,6 @@ print("Tên là: ",ten)
 ngaythangnamsinh = input("Ngày tháng năm sinh: ")
 print("Ngày tháng năm sinh: ",ngaythangnamsinh)
 
-a = int (input("Nhập a:"))
+
 b = int (input("Nhập b:"))
 print("a+b=",a+b)
